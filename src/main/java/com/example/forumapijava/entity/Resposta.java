7b4e61a6b98c7545c6c5023ee53e4fc1d0b3497a
robedj <ed.wassouf@gmail.com>
2022-01-31 18:42:03 -0400

    map. Jpa completo

--- src/main/java/com/example/forumapijava/entity/Resposta.java
+++ src/main/java/com/example/forumapijava/entity/Resposta.java
@@ -1,13 +1,18 @@
 package com.example.forumapijava.entity;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Resposta {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String mensagem;
+    @ManyToOne
     private Topico topico;
     private LocalDateTime dataCriacao = LocalDateTime.now();
+    @ManyToOne
     private Usuario autor;
     private Boolean solucao = false;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT into USUARIO(nome, email, senha) VALUES
+    ('Aluno', 'aluno@email.com', '123454');
+INSERT INTO CURSO(nome, categoria)
+  VALUES ('Springboot', 'Programação');
+INSERT INTO CURSO(nome, categoria)
+  VALUES ('html5', 'Front-End');
