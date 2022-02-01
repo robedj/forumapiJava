0b46c59cace0a08e06d00c9ccd5225b3e2b63de6
robedj <ed.wassouf@gmail.com>
2022-01-31 22:20:32 -0400

    Corrigindo bug - TopicosController

--- src/main/java/com/example/forumapijava/controller/TopicosController.java
+++ src/main/java/com/example/forumapijava/controller/TopicosController.java
@@ -1,28 +1,25 @@
 package com.example.forumapijava.controller;
-
-
 import java.util.Arrays;
 import java.util.List;
 import com.example.forumapijava.controller.dto.TopicoDto;
 import com.example.forumapijava.entity.Curso;
 import com.example.forumapijava.entity.Topico;
+import com.example.forumapijava.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
-@RequestMapping("/topicos") //endereco mapeado
 @RestController
 public class TopicosController {
 
-    @GetMapping
-    public List<TopicoDto> list() {
+    @Autowired
+    private TopicoRepository topicoRepository;
 
-        Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Spring","Programação"));
-
-        return TopicoDto.converter(Arrays.asList(topico,topico,topico));
-
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista() {
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
     }
-
 }
