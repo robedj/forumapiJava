44b93d06e4023b19fd99ede7806e417a8df91d64
robedj <ed.wassouf@gmail.com>
2022-02-01 15:25:22 -0400

    List<Topico> findByCurso_Nome(String nomeCurso);

--- src/main/java/com/example/forumapijava/repository/TopicoRepository.java
+++ src/main/java/com/example/forumapijava/repository/TopicoRepository.java
@@ -2,11 +2,17 @@ package com.example.forumapijava.repository;
 
 import com.example.forumapijava.entity.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TopicoRepository extends JpaRepository <Topico, Long>{
 
 
-    List<Topico> findByCursoNome(String nomeCurso);
+    List<Topico> findByCurso_Nome(String nomeCurso);
+
+
+    @Query("SELECT t FROM Topico T WHERE t.curso.nome = :nomeCurso")
+    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso")String nomeCurso);
 }
