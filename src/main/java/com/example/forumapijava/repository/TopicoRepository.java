e3127a7172e71e5d4e9f03db40f21f8db96cd675
robedj <ed.wassouf@gmail.com>
2022-01-31 22:09:27 -0400

    InterdaceJpa repository

---
+++ src/main/java/com/example/forumapijava/repository/TopicoRepository.java
@@ -0,0 +1,9 @@
+package com.example.forumapijava.repository;
+
+import com.example.forumapijava.entity.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository <Topico, Long>{
+
+
+}
